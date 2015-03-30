206e86cfd4732334056c724f41f68a741be6c00f
Beau Barker <beauinmelbourne@gmail.com>
2015-03-30 16:21:52 +1100

    create_app is no longer a staticmethod.

--- testingpython/testflask.py
+++ testingpython/testflask.py
@@ -6,6 +6,7 @@ def requires_db(db):
     """Decorator for setting up and tearing down the database"""
     def decorator(function_to_decorate):
         def wrapper(self):
+            db.drop_all()
             db.create_all()
             ret = function_to_decorate(self)
             db.session.remove()
@@ -16,8 +17,7 @@ def requires_db(db):
 
 class TestFlask(TestCase):
 
-    @staticmethod
-    def create_app(app):
+    def create_app(self, app):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
         app.config['TESTING'] = True
         return app
