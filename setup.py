5e15362d7e2f31195bdbe6629f35f7fb4720c91b
Beau Barker <beauinmelbourne@gmail.com>
2015-04-17 09:28:05 +1000

    Added testsqlalchemy to setup.py py_modules.

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     description='My own classes for testing python.',
     author='Beau Barker',
     author_email='beauinmelbourne@gmail.com',
-    py_modules=['testflask'],
+    py_modules=['testflask', 'testsqlalchemy'],
     tests_require=['tox'],
     )
