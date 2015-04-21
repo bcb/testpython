7a7af5939c392b5d960ab38bf637535e414ef74d
Beau Barker <beauinmelbourne@gmail.com>
2015-04-22 08:16:14 +1000

    Added login and logout functions.

--- testflask.py
+++ testflask.py
@@ -2,9 +2,21 @@
 
 from flask.ext.testing import TestCase
 
+
 class TestFlask(TestCase):
-    @staticmethod
-    def create_app(app):
+
+    def create_app(self, app):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
         app.config['TESTING'] = True
+        app.config['LOGIN_DISABLED'] = False
+        app.config['WTF_CSRF_ENABLED'] = False
         return app
+
+    def login(self, username='admin', password='admin'):
+        return self.client.post('/login', data=dict(
+            username=username,
+            password=password
+        ))
+
+    def logout(self):
+        return self.client.get('/logout')
