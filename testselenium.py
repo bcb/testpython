8cfec87ba2693d627d1fe0ae7a13b9cb1b871586
Beau Barker <beauinmelbourne@gmail.com>
2015-04-22 21:51:10 +1000

    Added login steps to testselenium.py.

--- testselenium.py
+++ testselenium.py
@@ -2,14 +2,14 @@
 #pylint:disable=unused-wildcard-import
 
 import re
-import time
+from time import sleep
 
 from behave import * #pylint:disable=wildcard-import
 
 
 def assert_uri(context, string):
     """Ensure the uri is correct"""
-    time.sleep(1)
+    sleep(0.5)
     assert re.search(string, context.browser.current_url)
 
 
@@ -22,12 +22,63 @@ def assert_heading(context, string):
 
 def assert_title(context, string):
     """Ensure the title is correct"""
+    sleep(0.5)
     assert re.search(string, context.browser.title)
+
+
+# Given
+@given('I\'ve not logged in')
+def step_impl(context):
+    context.browser.get('http://localhost/logout')
+
+
+@given('I\'ve logged in')
+def step_impl(context):
+    context.browser.get('http://localhost/login')
+    context.execute_steps('''
+        When I login
+    ''')
+
+
+# When
+@when('I visit the login page')
+def step_impl(context):
+    context.browser.get('http://localhost/login')
+
+
+@when('I visit the logout page')
+def step_impl(context):
+    context.browser.get('http://localhost/logout')
+
+
+@when('I login')
+def step_impl(context):
+    context.browser.find_element_by_xpath('//input[@name="username"]') \
+        .send_keys('admin')
+    context.browser.find_element_by_xpath('//input[@name="password"]') \
+        .send_keys('pass')
+    context.browser.find_element_by_xpath('//input[@name="submit"]').click()
+
+
+@when('I attempt a failed login')
+def step_impl(context):
+    context.browser.find_element_by_xpath('//input[@name="username"]') \
+        .send_keys('noname')
+    context.browser.find_element_by_xpath('//input[@name="password"]') \
+        .send_keys('nopass')
+    context.browser.find_element_by_xpath('//input[@name="submit"]').click()
+
+
+# Then
+@then('I should see the login page')
+def step_impl(context):
+    assert_uri(context, '/login')
+    assert_heading(context, 'Login')
 
 
 @then('I should see the message "{text}"')
 @then('I should see "{text}"')
 def step_impl(context, text):
     """Checks a flash message or other text in the page"""
-    time.sleep(1)
+    sleep(0.5)
     assert text in context.browser.page_source
